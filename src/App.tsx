423f56100d036f4120963fa7064f1ef48a570b3f
Fernando200519 <zs23021811@estudiantes.uv.mx>
2025-10-25 09:36:39 -0600

    Actualizar main

--- src/App.tsx
+++ src/App.tsx
@@ -4,29 +4,12 @@ import {
   Route,
   Navigate,
 } from "react-router-dom";
-import { useAuth } from "./context/AuthContext";
-import Login from "./pages/Login";
-import Dashboard from "./pages/Dashboard";
-import { JSX } from "react";
-
-const ProtectedRoute = ({ children }: { children: JSX.Element }) => {
-  const { token } = useAuth();
-  return token ? children : <Navigate to="/login"></Navigate>;
-};
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/login" element={<Login />} />
-        <Route
-          path="/dashboard"
-          element={
-            <ProtectedRoute>
-              <Dashboard />
-            </ProtectedRoute>
-          }
-        ></Route>
+        <Route></Route>
         <Route path="*" element={<Navigate to="/login" />}></Route>
       </Routes>
     </Router>
