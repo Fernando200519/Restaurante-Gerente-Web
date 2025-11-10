a57dcc99612cbe8fc1535db29dacc65875529920
Fernando200519 <zs23021811@estudiantes.uv.mx>
2025-11-09 22:58:49 -0600

    Login responsive

--- src/components/LoginForm.tsx
+++ src/components/LoginForm.tsx
@@ -27,28 +27,28 @@ const LoginForm: React.FC = () => {
   return (
     <form
       onSubmit={handleSubmit}
-      className="bg-white w-full max-w-sm sm:max-w-md md:max-w-lg mx-auto"
+      className="bg-white w-full max-w-lg xl:max-w-xl 2xl:max-w-2xl mx-auto p-6 xl:p-10 2xl:p-12"
     >
-      <h2 className="mb-8 sm:mb-10 font-inter text-3xl sm:text-4xl font-bold text-gray-800">
+      <h2 className="mb-10 2xl:mb-12 font-inter text-3xl 2xl:text-4xl font-bold text-gray-800">
         Iniciar sesión
       </h2>
 
       {error && (
-        <p className="text-lg sm:text-xl text-red-500 font-semibold mb-8 sm:mb-10">
+        <p className="text-lg 2xl:text-xl text-red-500 font-semibold mb-8 2xl:mb-10">
           {error}
         </p>
       )}
 
-      <div className="mb-6">
-        <label className="block font-inter text-lg sm:text-xl text-gray-600 font-semibold mb-3">
+      <div className="mb-6 2xl:mb-7">
+        <label className="block font-inter text-lg 2xl:text-xl text-gray-600 font-semibold mb-3 2xl:mb-4">
           Usuario
         </label>
         <div className="relative">
-          <div className="absolute inset-y-0 left-0 flex items-center pl-4 pointer-events-none">
-            <img src="/user_icon.svg" className="h-6 w-6" />
+          <div className="absolute inset-y-0 left-0 flex items-center pl-4 2xl:pl-5 pointer-events-none">
+            <img src="/user_icon.svg" className="h-6 w-6 2xl:h-7 2xl:w-7" />
           </div>
           <input
-            className="w-full border border-gray-300 rounded-lg h-[55px] sm:h-[65px] text-lg sm:text-xl pl-14 pr-14 focus:outline-none focus:ring-2 focus:ring-orange-400"
+            className="w-full border border-gray-300 rounded-lg h-[55px] 2xl:h-16 text-lg 2xl:text-xl pl-14 2xl:pl-16 pr-14 2xl:pr-16 focus:outline-none focus:ring-2 focus:ring-orange-400"
             type="text"
             placeholder="Ingresa tu nombre de usuario"
             value={user}
@@ -58,16 +58,16 @@ const LoginForm: React.FC = () => {
         </div>
       </div>
 
-      <div className="mb-10 sm:mb-14">
-        <label className="block font-inter text-lg sm:text-xl text-gray-600 font-semibold mb-3">
+      <div className="mb-10 2xl:mb-12">
+        <label className="block font-inter text-lg 2xl:text-xl text-gray-600 font-semibold mb-3 2xl:mb-4">
           Contraseña
         </label>
         <div className="relative">
-          <div className="absolute inset-y-0 left-0 flex items-center pl-4 pointer-events-none">
-            <img src="/password_icon.svg" className="h-6 w-6" />
+          <div className="absolute inset-y-0 left-0 flex items-center pl-4 2xl:pl-5 pointer-events-none">
+            <img src="/password_icon.svg" className="h-6 w-6 2xl:h-7 2xl:w-7" />
           </div>
           <input
-            className="w-full border border-gray-300 rounded-lg h-[55px] sm:h-[65px] text-lg sm:text-xl pl-14 pr-14 focus:outline-none focus:ring-2 focus:ring-orange-400"
+            className="w-full border border-gray-300 rounded-lg h-[55px] 2xl:h-16 text-lg 2xl:text-xl pl-14 2xl:pl-16 pr-14 2xl:pr-16 focus:outline-none focus:ring-2 focus:ring-orange-400"
             type={isPasswordVisible ? "text" : "password"}
             value={password}
             placeholder="Ingresa tu contraseña"
@@ -78,12 +78,12 @@ const LoginForm: React.FC = () => {
             <button
               type="button"
               onClick={togglePasswordVisibility}
-              className="absolute inset-y-0 right-0 flex items-center pr-3"
+              className="absolute inset-y-0 right-0 flex items-center pr-3 2xl:pr-4"
             >
               <img
                 src={isPasswordVisible ? "/eye_closed.svg" : "/eye_open.svg"}
                 alt="Toggle password"
-                className="h-8 w-8 sm:h-9 sm:w-9"
+                className="h-8 w-8 2xl:h-9 2xl:w-9 cursor-pointer"
               />
             </button>
           )}
@@ -92,7 +92,7 @@ const LoginForm: React.FC = () => {
 
       <button
         type="submit"
-        className="w-full bg-[#FF8108] hover:bg-[#e76e07] text-white font-bold text-xl sm:text-2xl rounded-lg h-[60px] sm:h-[70px] transition-all"
+        className="w-full bg-[#FF8108] hover:bg-[#e76e07] text-white font-bold text-xl 2xl:text-2xl rounded-lg h-[60px] 2xl:h-18 transition-all cursor-pointer"
       >
         Iniciar sesión
       </button>
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -4,17 +4,17 @@ import LoginForm from "../components/LoginForm";
 const Login: React.FC = () => {
   return (
     <div className="flex flex-col lg:flex-row h-screen">
-      <div className="hidden lg:flex lg:w-1/2 flex-col justify-center items-center bg-[#FA9623] text-center p-10">
-        <h1 className="font-poppins font-bold text-4xl xl:text-5xl text-white mb-4">
-          Título del restaurante
+      <div className="hidden lg:flex lg:w-1/2 flex-col justify-center items-center bg-[#FA9623] text-center p-10 xl:p-20 2xl:p-28">
+        <h1 className="font-poppins font-bold text-4xl xl:text-5xl text-white mb-6 tracking-tight">
+          Mesa Libre
         </h1>
-        <h2 className="text-white font-medium text-xl xl:text-2xl max-w-md">
+        <h2 className="text-white font-medium text-xl xl:text-2xl max-w-xl">
           Sistema integral para la gestión de tu restaurante.
         </h2>
       </div>
 
-      <div className="flex justify-center items-center bg-white w-full lg:w-1/2 p-6 sm:p-10">
-        <div className="w-full max-w-md sm:max-w-lg md:max-w-md">
+      <div className="flex justify-center items-center bg-white w-full lg:w-1/2 p-8 xl:p-12">
+        <div className="w-full max-w-md md:max-w-lg xl:max-w-xl 2xl:max-w-2xl">
           <LoginForm />
         </div>
       </div>
